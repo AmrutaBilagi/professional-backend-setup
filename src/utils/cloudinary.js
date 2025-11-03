bbe9cad0a02a3177e1754222a7043206a89d51be
AmrutaBilagi <amrutabilagi8@gmal.com>
2025-11-03 17:41:45 +0530

    cloudinary,api key,secret,name,multer and cloudinary dependency installed,middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+})
+
+const uploadOncloudinary=async(localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+        //upload file on cloudinary
+       const response=await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file has been uploaded successfully
+        console.log("File is uploaded successfully",response.url);
+        return response;
+    } catch (error) {   //if not uploaded ,file has to be unlink
+        fs.unlinkSync(localFilePath) //fs=>filesystem (inbuilt)
+         //remove the locally saved temprary file as the upload operation got failed
+        return null
+    }
+}
+export {uploadOncloudinary}
+
+// cloudinary.v2.uploader
+// .upload("dog.mp4", {
+//   resource_type: "video", 
+//   public_id: "my_dog",
+//   overwrite: true, 
+//   notification_url: "https://mysite.example.com/notify_endpoint"})
+// .then(result=>console.log(result));
